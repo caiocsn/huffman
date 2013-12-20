5b9c8cd0274c738ce52bc2e1de79b6ce47d6e7a5
larissartemis <larissartemis@localhost.localdomain>
2013-12-19 23:27:00 -0300

    Read and write with files

--- huffman/main.cpp
+++ huffman/main.cpp
@@ -1,8 +1,18 @@
 #include <File.h>
+#include <QDebug>
+#include <QByteArray>
 
 int main(int argc, char *argv[])
 {
     File * f = new File("/home/larissartemis/workspace/FileTest/", "teste.txt");
-    f->split();
+    // f->split();
+
+    QByteArray q = f->read();
+    QString s;
+    s.append(q);
+
+    f->write(q);
+
+    qDebug() << "done";
     return 0;
 }
